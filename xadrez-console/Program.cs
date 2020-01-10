3eb5a3f0c038fc939a74055866d5cbc272fb6afb
Kaue Sloboda <kaue.sloboda@hotmail.com>
2020-01-10 14:01:55 -0300

    Implementando a logica de xequemate

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -53,7 +53,10 @@ namespace xadrez_console
                     }
                 }
 
-                Tela.imprimirTabuleiro(partida.tab);
+                Console.Clear();
+                Tela.imprimirPartida(partida);
+
+                //Tela.imprimirTabuleiro(partida.tab);
             }
             catch (TabuleiroException e)
             {
